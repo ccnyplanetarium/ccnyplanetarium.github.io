bf5d0abaeb00d730113fcde27c7c88e1786d7b88
James Hedberg <jhedberg@ccny.cuny.edu>
2021-04-17 06:49:55 -0400

    add some more

--- assets/dynamic-media/globes/globe-data.js
+++ assets/dynamic-media/globes/globe-data.js
@@ -1,4 +1,6 @@
 export const array = [
   ['earth-jah','.jpg','JAH','04-13-2021',"Earth",1,'001'],
-  ['earth-vmh','.jpg','VMH','04-13-2021',"Earth",2,'002']
+  ['earth-vmh','.jpg','VMH','04-13-2021',"Earth",2,'002'],
+  ['eb-003','.jpg','Eleanor B','04-17-2021',"Earth",3,'003'],
+  ['sb-004','.jpg','Sebastian B','04-17-2021',"Earth",4,'004']
 ];
